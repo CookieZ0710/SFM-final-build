06366e8e577b74b532a2055db08ab40da4ce44d3
CookieZ0710 <105343619+CookieZ0710@users.noreply.github.com>
2023-11-06 02:02:50 +0800

    Delete SFM/Management directory

--- SFM/Management/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","sfm");
-
-// Check connection
-if (mysqli_connect_error()){
-    echo "Failed to connect to DB".mysqli_connect_error();
-}
-
-?>
--- SFM/Management/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["UserID"]))
-{
-header("location: /SFM/Main/Loginpage.php");
-}
-?>

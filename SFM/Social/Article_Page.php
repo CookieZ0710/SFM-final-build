e8bb9170eb8126eeb7bbdd67824e6b67b88df006
CookieZ0710 <105343619+CookieZ0710@users.noreply.github.com>
2023-11-06 22:31:38 +0800

    Update Article_Page.php

--- SFM/Social/Article_Page.php
+++ SFM/Social/Article_Page.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+        include("session.php");
+        include ("conn.php");
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Social hub</title>
